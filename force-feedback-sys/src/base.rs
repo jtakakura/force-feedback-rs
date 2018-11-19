c3336e81eb992745cd7d337ffe510b8454f49884
Junji Takakura <j.takakura@gmail.com>
2018-11-20 06:49:39 +0900

    Add base types

---
+++ force-feedback-sys/src/base.rs
@@ -0,0 +1,9 @@
+// exports from `MacTypes.h`
+pub type UInt32 = ::std::os::raw::c_uint;
+pub type SInt32 = ::std::os::raw::c_int;
+
+// Version Release Stage Codes
+pub const developStage: u32 = 0x20;
+pub const alphaStage: u32 = 0x40;
+pub const betaStage: u32 = 0x60;
+pub const finalStage: u32 = 0x80;
